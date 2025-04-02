982f885f202fc5e8293b990058f5493d17c2b311
Lorenzo Fanton <lorenzo.fanton@outlook.it>
2025-04-02 21:44:45 +0200

    [wip] Ugly Mesh3 test-and-error

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,38 @@
-import {Provider, Wallet} from './config';
+import {Wallet} from './config';
+import buildOutput from "./simple/plutus.json";
+import {Data, PlutusScript, resolvePlutusScriptAddress, Transaction} from "@meshsdk/core";
 
+const scriptCbor = buildOutput.validators[0].compiledCode;
+
+const script: PlutusScript = {
+  code: scriptCbor,
+  version: 'V3',
+};
+
+const scriptAddress = resolvePlutusScriptAddress(script, 0);
+console.log(scriptAddress);
 
 async function main() {
-  return await Provider.fetchAccountInfo(Wallet.getChangeAddress());
+  const addr = Wallet.getUsedAddress();
+
+  const datum: Data = {
+    alternative: 0,
+    fields: [],
+  };
+
+  const tx = new Transaction({initiator: Wallet})
+    .sendLovelace(
+      {
+        address: scriptAddress,
+        datum: {
+          value: datum,
+        },
+      },
+      "1000000",
+    );
+  const unsignedTx = await tx.build();
+  const signedTx = Wallet.signTx(unsignedTx, true);
+  return await Wallet.submitTx(signedTx);
 }
 
 main()
