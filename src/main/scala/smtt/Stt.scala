8d2e956578585b9a7b5a6950874bd87422f1fa8c
Lorenzo Fanton <lorenzo.fanton@outlook.it>
2025-10-07 17:19:02 +0200

    [refactor] Using proper monadic style

--- src/main/scala/smtt/Stt.scala
+++ src/main/scala/smtt/Stt.scala
@@ -1,38 +1,28 @@
 package smtt
 
 import scalus.*
-import scalus.cardano.ledger.AssetName
+import scalus.builtin.ByteString
 import scalus.ledger.api.v3.*
-import scalus.prelude.Option.Some
+import scalus.prelude.Option.{None, Some}
 import scalus.prelude.{*, given}
 
 @Compile
 object Stt extends ParameterizedValidator[TxOutRef]:
-    override def mint(
-        txOutRef: TxOutRef,
-        _redeemer: Redeemer,
-        policyId: PolicyId,
-        txInfo: TxInfo
-    ): Unit =
-        val inputs = txInfo.inputs
-        val outputs = txInfo.outputs
-        val mint = txInfo.mint
-
-        // JUST IMPLEMENT https://aiken-lang.org/fundamentals/common-design-patterns#state-thread-tokens-aka-stt
-        mint.toSortedMap
-
-        if !inputParamIsSpent(txInfo.inputs, txOutRef) then
-            fail("The output reference parameter is not consumed by the minting transaction.")
-        val mintedTokens = getOutputMintedTokens(txInfo.outputs, policyId)
-
-    private def inputParamIsSpent(inputs: List[TxInInfo], txOutRef: TxOutRef): Boolean =
-        Utils.findInput(inputs, txOutRef) match
-            case Some(_) => true
-            case _       => false
-
-    private def getOutputMintedTokens(
-        outputs: List[TxOut],
-        policyId: PolicyId
-    ): SortedMap[AssetName, Long] = {
-        fail("TODO: implement")
-    }
+  // Ported from Aiken's State Thread Tokens example:
+  // https://aiken-lang.org/fundamentals/common-design-patterns#state-thread-tokens-aka-stt
+  override def mint(
+      txOutRef: TxOutRef,
+      _redeemer: Redeemer,
+      policyId: PolicyId,
+      txInfo: TxInfo
+  ): Unit =
+    val result: Option[Unit] = for {
+      // Ensure the UTxO identified by `txOutRef` is consumed by this transaction.
+      input <- Utils.findInput(txInfo.inputs, txOutRef)
+      // Ensure exactly one "STT" token is minted and nothing else.
+      tokens <- txInfo.mint.toSortedMap.get(policyId)
+      tokens <- if tokens.size == BigInt(1) then Some(tokens) else None
+      amount <- tokens.get(ByteString.fromString("STT"))
+      result <- if amount == BigInt(1) then Some(()) else None
+    } yield result
+    result.getOrElse(fail("Could not find the required minting conditions."))
