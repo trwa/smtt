ae8776210c6bd0c5d9c9a48a094d6bfaa0bdf1a7
Lorenzo Fanton <lorenzo.fanton@chronos-tech.it>
2025-12-11 11:11:28 +0100

    Refactor STT.scala

--- src/main/scala/smtt/STT.scala
+++ src/main/scala/smtt/STT.scala
@@ -3,30 +3,30 @@ package smtt
 import scalus.*
 import scalus.builtin.ByteString
 import scalus.ledger.api.v3.*
+import scalus.prelude.*
 import scalus.prelude.Option.{None, Some}
-import scalus.prelude.{*, given}
 
 @Compile
 object STT extends ParameterizedValidator[TxOutRef]:
   // Ported from Aiken's State Thread Tokens example:
   // https://aiken-lang.org/fundamentals/common-design-patterns#state-thread-tokens-aka-stt
   override def mint(
-      oneTimeInvocationProof: TxOutRef,
-      _r: Redeemer,
-      sttPolicyId: PolicyId,
+      paramOneTimeInvocationProof: TxOutRef,
+      _redeemer: Redeemer,
+      policyId: PolicyId,
       txInfo: TxInfo
   ): Unit = (
-    for
+    for {
       // Ensure the UTxO identified by `txOutRef` is consumed by this transaction.
-      input <- Utils.findInput(txInfo.inputs, oneTimeInvocationProof)
+      input <- Utils.findInput(txInfo.inputs, paramOneTimeInvocationProof)
       // Ensure exactly one "STT" token is minted from this policyId and nothing else.
-      tokens <- txInfo.mint.toSortedMap.get(sttPolicyId)
-      tokens <- if tokens.size == BigInt(1) then Some(tokens) else None
-      sttAssetName = ByteString.fromString("STT")
-      amount <- tokens.get(sttAssetName)
+      mintedTokens <- txInfo.mint.toSortedMap.get(policyId)
+      mintedTokens <- if mintedTokens.size == BigInt(1) then Some(mintedTokens) else None
+      assetName = ByteString.fromString("STT")
+      amount <- mintedTokens.get(assetName)
       result <- if amount == BigInt(1) then Some(()) else None
-    // Ensure the output containing the "STT" token has empty datum.
-    /* TODO: REMOVE! Not so useful...
+      // Ensure the output containing the "STT" token has empty datum.
+      /* TODO: REMOVE! Not so useful...
       sttRecipient <- transaction.outputs.find(output =>
         val found = for
           tokens <- output.value.toSortedMap.get(sttPolicyId)
@@ -38,6 +38,6 @@ object STT extends ParameterizedValidator[TxOutRef]:
         case OutputDatum(datum) => Some(datum.to[RunDatum])
         case _                  => None
       valid <- if !datum.started then Some(()) else None
-     */
-    yield result
+       */
+    } yield result
   ).getOrElse(fail("Could not find the required minting conditions."))
