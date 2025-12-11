7a0b74bbcba3a81bd862e7c88ede50fd59d59ee0
Lorenzo Fanton <lorenzo.fanton@chronos-tech.it>
2025-12-11 11:11:39 +0100

    Implement Tag validators

--- src/main/scala/smtt/Tag.scala
+++ src/main/scala/smtt/Tag.scala
@@ -1,28 +1,42 @@
 package smtt
 
 import scalus.*
-import scalus.builtin.Data
+import scalus.builtin.{ByteString, Data}
 import scalus.ledger.api.v3.*
-import scalus.prelude.{*, given}
+import scalus.prelude.*
+import scalus.prelude.Option.{None, Some}
 
 @Compile
 object Tag extends ParameterizedValidator[PolicyId] {
-    override def mint(
-        sttPolicyId: PolicyId,
-        redeemer: Redeemer,
-        policyId: PolicyId,
-        txInfo: TxInfo
-    ): Unit = {
-        fail("TODO: implement Tag")
-    }
+  // Ensure "STT" is in inputs (delegate all logic to Run validator)
+  override def mint(
+      paramSttPolicyId: PolicyId,
+      _redeemer: Redeemer,
+      _policyId: PolicyId,
+      txInfo: TxInfo
+  ): Unit =
+    checkSttIsInInputs(txInfo, paramSttPolicyId)
 
-    override def spend(
-        sttPolicyId: PolicyId,
-        datum: Option[Data],
-        redeemer: Redeemer,
-        txInfo: TxInfo,
-        ownOutRef: TxOutRef
-    ): Unit = {
-        fail("TODO: implement Tag")
-    }
+  private def checkSttIsInInputs(txInfo: TxInfo, sttPolicyId: PolicyId): Unit = {
+    if txInfo.inputs
+        .filter(input => {
+          input.resolved.value.toSortedMap.get(sttPolicyId) match
+            case None => false
+            case Some(tokens) =>
+              tokens.get(ByteString.fromString("STT")) match
+                case None         => false
+                case Some(amount) => amount == BigInt(1)
+        })
+        .length != BigInt(1)
+    then fail("STT token holder not found or multiple found")
+  }
+
+  // Ensure "STT" is in inputs (delegate all logic to Run validator)
+  override def spend(
+      sttPolicyId: PolicyId,
+      _datum: Option[Data],
+      _redeemer: Redeemer,
+      txInfo: TxInfo,
+      _ownOutRef: TxOutRef
+  ): Unit = checkSttIsInInputs(txInfo, sttPolicyId)
 }
